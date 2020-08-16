6601672e0133289e41bc85dc2f408d513f6ba569
SabrineBaam <baameur.sabrine@gmail.com>
2020-08-16 13:24:09 +0100

    Updates

---
+++ precache-manifest.c5c763ca56f8e1284fd795d1086ab1f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b0e12c495f8f793e58a2f33c3b7896a",
+    "url": "/Drum-Machine/index.html"
+  },
+  {
+    "revision": "76770e3e8f3dbd13528c",
+    "url": "/Drum-Machine/static/css/main.a05117bb.chunk.css"
+  },
+  {
+    "revision": "0f238e793850b77281d8",
+    "url": "/Drum-Machine/static/js/2.ffd68878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drum-Machine/static/js/2.ffd68878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76770e3e8f3dbd13528c",
+    "url": "/Drum-Machine/static/js/main.e14f7c62.chunk.js"
+  },
+  {
+    "revision": "1b4da9fef87ef5fe97c9",
+    "url": "/Drum-Machine/static/js/runtime-main.1603ff6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Drum-Machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
